95d24f4285159b9e6bba54e67eb15d0e533a7b71
Priyanshu Shrivastava <125429914+Priyanshu4002@users.noreply.github.com>
2023-11-01 11:37:46 +0530

    fix backend

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './styles/app.scss';
 import { createContext } from 'react';
 
-export const server = "https://to-do-webapp-wwau.onrender.com";
+export const server = "https://to-do-backend-dr41.onrender.com";
 
 export const Context = createContext({ isAuthenticated: false });
 
